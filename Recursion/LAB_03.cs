e7c255e5f3de7de757f0acc849a01d63a720f596
Gia Vũ <danglegiavu@gmail.com>
2019-05-05 18:44:57 +0700

    Update LAB_03.cs

--- Recursion/LAB_03.cs
+++ Recursion/LAB_03.cs
@@ -14,40 +14,39 @@ namespace ConsoleApp1
         static string line;
         static string[] tokens;
         static List<string> res = new List<string>();
-        public static void TowerOfHanoi(int n,char a,char b,char c)
+        public static void TowerOfHanoi(int n, char a, char b, char c)
         {
             if (n == 1)
-            {           
-                line="Chuy?n ??a t? c?t " + a + " sang c?t " + c;                
-                res.Add(line);                  
-                return;               
+            {
+                line = "Chuyển đĩa từ cột " + a + " sang cột " + c;
+                res.Add(line);
+                return;
             }
             TowerOfHanoi(n - 1, a, c, b);
             TowerOfHanoi(1, a, b, c);
             TowerOfHanoi(n - 1, b, a, c);
         }
-        
+
 
         static void Main(string[] args)
         {
-           
-        
+
+
             using (StreamReader myFile = new StreamReader(fileInput))
             {
-                line = myFile.ReadLine();           
+                line = myFile.ReadLine();
                 int n = int.Parse(line);
                 char a = 'A', b = 'B', c = 'C';
                 TowerOfHanoi(n, a, b, c);
-                using (StreamWriter outFile=new StreamWriter(fileOutput))
+                using (StreamWriter outFile = new StreamWriter(fileOutput))
                 {
                     foreach (string step in res)
                     {
                         outFile.WriteLine(step);
                     }
                 }
-              
+
             }
         }
     }
 }
-
