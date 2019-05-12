d106278617710a80a510423410055b2e447ebede
Gia Vũ <danglegiavu@gmail.com>
2019-05-12 18:32:46 +0700

    Update Program.cs

--- OOP/Inheritance_Polymorphism/Program.cs
+++ OOP/Inheritance_Polymorphism/Program.cs
@@ -4,18 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LAB8
+namespace ConsoleApp2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Person person = new Person("DangLeGiaVu", "New York");
-            Student student = new Student(person.getName,person.getAddress,"Math", 2000, 155.5);
-            Staff staff = new Staff(person.getName, person.getAddress, "HUFLIT", 123.5);
-            Console.WriteLine(person.toString);
-            Console.WriteLine(student.toString);
-            Console.WriteLine(staff.toString);
+            #region Person-Staff-Student
+            //Person person = new Person("DangLeGiaVu", "New York");
+            //Student student = new Student(person.getName, person.getAddress, "Math", 2000, 155.5);
+            //Staff staff = new Staff(person.getName, person.getAddress, "HUFLIT", 123.5);
+            //Console.WriteLine(person.toString);
+            //Console.WriteLine(student.toString);
+            //Console.WriteLine(staff.toString);
+            #endregion
+            #region Shape-Circle-Rectangle
+            //Circle circle = new Circle(1, "black", true);
+            //Rectangle rectangle = new Rectangle(1, 2);
+            //Square square = new Square(5);
+            //Console.Write(circle.toString());
+
+            //circle.setRadius(1);
+            //Console.WriteLine(circle.getArea());
+            //Console.WriteLine(circle.getPerimeter());
+            //Console.WriteLine(circle.toString());
+            //Console.Write(rectangle.toString());
+
+        
+            #endregion
+
         }
     }
 }
