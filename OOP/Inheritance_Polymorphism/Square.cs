9cf8233671ee7f9b8c8e28ea291669624a157060
Gia Vũ <danglegiavu@gmail.com>
2019-05-12 18:35:08 +0700

    Create Square.cs

---
+++ OOP/Inheritance_Polymorphism/Square.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Square:Rectangle
+    {
+        private double side;
+
+        public Square()
+        {
+
+        }
+
+        public Square(double side)
+        {
+            this.side = side;
+        }
+
+        public Square(double side,string color,bool filled):base(color,filled)
+        {
+            this.side = side;
+        }
+
+        public double getSide()
+        {
+            return side;
+        }
+
+        public void setSide(double side)
+        {
+            this.side = side;
+        }
+
+        public override void setWidth(double side)
+        {
+             base.setWidth(side);
+        }
+
+        public override void setLength(double side)
+        {
+            base.setLength(side);
+        }
+
+        public override string toString()
+        {
+            return "I'm a Square";
+        }
+
+    }
+}
