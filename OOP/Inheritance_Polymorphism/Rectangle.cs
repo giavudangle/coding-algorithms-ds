9e7cd8d2eff1ba3316d810ce6dee50fec91927d3
Vu Dang <danglegiavu@gmail.com>
2019-05-17 01:19:02 +0700

    SUBMIT

--- OOP/Inheritance_Polymorphism/Rectangle.cs
+++ OOP/Inheritance_Polymorphism/Rectangle.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// Created by Chicken_Coder
 namespace ConsoleApp2
 {
     class Rectangle:Shape
