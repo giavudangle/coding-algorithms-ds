8a455ffe8a56bcee6c9e09d963ef0d8aaece4eef
Vu Dang <danglegiavu@gmail.com>
2019-11-07 03:51:04 +0700

    Update Program.cs

--- DataAndAlgorithm/BinarySearchTree/GenericsVersion/Program.cs
+++ DataAndAlgorithm/BinarySearchTree/GenericsVersion/Program.cs
@@ -10,6 +10,7 @@ namespace Tree
     {
         static void Main(string[] args)
         {
+            #region Create Binary Tree
             Console.ForegroundColor = ConsoleColor.Green;
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
@@ -17,23 +18,10 @@ namespace Tree
             //MyBinaryTree<char> myTree = new MyBinaryTree<char>();
             //MyBinaryTree<string> myTree = new MyBinaryTree<string>();
             //MyBinaryTree<double> myTree = new MyBinaryTree<double>();
+            #endregion
 
+            #region Insert Mock Data
 
-            //myTree.Insert(25);
-            //myTree.Insert(15);
-            //myTree.Insert(50);
-            //myTree.Insert(10);
-            //myTree.Insert(22);
-            //myTree.Insert(35);
-            //myTree.Insert(70);
-            //myTree.Insert(4);
-            //myTree.Insert(12);
-            //myTree.Insert(18);
-            //myTree.Insert(24);
-            //myTree.Insert(31);
-            //myTree.Insert(44);
-            //myTree.Insert(66);
-            //myTree.Insert(90);
 
             //myTree.Insert(25);
             //myTree.Insert(19);
@@ -43,22 +31,13 @@ namespace Tree
             //myTree.Insert(27);
             //myTree.Insert(55);
 
-            
-            myTree.Insert(1);
-            myTree.Insert(2);
-            myTree.Insert(3);
-            myTree.Insert(4);
-            myTree.Insert(5);
-            myTree.Insert(6);
 
-
-            //MyTNode<int> node = myTree.Root;
-            
-
-
-            //Console.WriteLine(myTree.LeafCount);
-            
-            //myTree.ListByLevel(node);
+            //myTree.Insert(1);
+            //myTree.Insert(2);
+            //myTree.Insert(3);
+            //myTree.Insert(4);
+            //myTree.Insert(5);
+            //myTree.Insert(6);
 
             //myTree.Insert('H');
             //myTree.Insert('B');
@@ -86,22 +65,37 @@ namespace Tree
             //myTree.Insert("Hải Dương");
 
 
-            //Console.WriteLine(myTree.Height);
-
-            //Console.WriteLine(myTree.Length);
-            //Console.WriteLine(myTree.Height);
-
-            //myTree.HeightTree(a);
-
-
             //myTree.Insert(1.5);
             //myTree.Insert(1.1); 
             //myTree.Insert(2.1);
             //myTree.Insert(2.2);
             //myTree.Insert(1.4);
+            #endregion
 
+            #region Properties Call
+            //Console.WriteLine(myTree.LeafCount);
+            //Console.WriteLine(myTree.Height);
+            #endregion
 
+            #region Method On Binary Search Tree
+            /* Thêm Node vào Cây */
+            //myTree.Insert(25);
+            //myTree.Insert(15);
+            //myTree.Insert(50);
+            //myTree.Insert(10);
+            //myTree.Insert(22);
+            //myTree.Insert(35);
+            //myTree.Insert(70);
+            //myTree.Insert(4);
+            //myTree.Insert(12);
+            //myTree.Insert(18);
+            //myTree.Insert(24);
+            //myTree.Insert(31);
+            //myTree.Insert(44);
+            //myTree.Insert(66);
+            //myTree.Insert(90);
 
+            /* Duyệt cây */
             //Console.Write("NLR: ");
             //myTree.PreOrder();
             //Console.WriteLine();
@@ -114,9 +108,9 @@ namespace Tree
             //myTree.PostOrder();
             //Console.WriteLine();
 
-            //MyTNode<int> node = new MyTNode<int>(0);
+            /* Tìm Node có trong cây không ? */
+            //MyTNode<int> node = new MyTNode<int>(15);
             //node = myTree.SearchNode(15);
-
             //if (node != null)
             //{
             //    Console.WriteLine("FOUND");
@@ -125,11 +119,11 @@ namespace Tree
             //else
             //    Console.WriteLine("NOT FOUND");
 
-            //MyTNode<string> maxNode = new MyTNode<string>("");
-            //MyTNode<string> minNode = new MyTNode<string>("");
+            /* Min Max Node của cây */
+            //MyTNode<int> maxNode = new MyTNode<int>();
+            //MyTNode<int> minNode = new MyTNode<int>();
             //maxNode = myTree.MaxNode();
             //minNode = myTree.MinNode();
-
             //if (maxNode != null)
             //    Console.WriteLine("Max Node -> " + maxNode.Data);
             //else
@@ -140,8 +134,12 @@ namespace Tree
             //else
             //    Console.WriteLine("Node is NULL");
 
-
-
+            /* Xóa Node trong cây */
+            //myTree.Remove(90);
+            //Console.Write("NLR: ");
+            //myTree.PreOrder();
+            //Console.WriteLine();
+            #endregion
         }
     }
 }
